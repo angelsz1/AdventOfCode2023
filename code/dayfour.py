b2b9a07dd8a8b0f6d60ebfd9778960f91d21ef59
Angel <eliasszust@gmail.com>
2023-12-06 02:19:10 -0300

    dayfour_p2 first attempt

--- code/dayfour.py
+++ code/dayfour.py
@@ -30,6 +30,28 @@ class DayFour(Solution):
         return points
 
     def part_two(self):
-        pass
+        self.lines = self.input.split('\n')
+        self.lines = [line for line in self.lines if line != '']
+        amounts = []
+        for line in self.lines:
+            hands = line.split(':')[1].split('|')
+            hands = [hand.strip() for hand in hands]
+            card_numbers = set(hands[0].split(' '))
+            scratch_numbers = set(hands[1].split(' '))
+            self.size = len(card_numbers) + len(scratch_numbers)
+            card_numbers.update(scratch_numbers)
+            winning_numbers = self.size - len(card_numbers)
+            amounts.append(winning_numbers)
+        card_copies = [1 for _ in range(0, len(self.lines))]
+        index = 0
+        for amount in amounts:
+            num_value = card_copies[index]
+            for i in range(index + 1, index + amount + 1):
+                print(i)
+                card_copies[i] += num_value
+            index += 1
+        return functools.reduce(lambda x, y: x + y, card_copies)
+
+
 
 DayFour().run(sys.argv[1])
